74ccd53ef5226b7210b7437a926279f59654f0bd
Kevin Matthews <kmatthews@cyberdata.net>
2018-06-01 11:39:52 -0700

    Add print options to script to verify csr

--- verify_csr.sh
+++ verify_csr.sh
@@ -3,22 +3,55 @@
 usage ()
 {
     echo ""
-    echo "Read Certificate Signing Request"
+    echo "Verify Certificate Signing Request"
     echo ""
-    echo "$(basename $0) [CSR]"
+    echo "$(basename $0) [OPTION] [CSR]"
     echo ""
-    echo "TODO add options"
+    echo "Options:"
+    echo "  --print-csr     Print the contens of the CSR"
     echo ""
 }
 
-CSR="$1"
+CSR=
+PRINT_CSR="-noout"      # Do not print by default
 
-if [ -z $CSR ]; then
-    echo "$(basename $0): Invalid CSR"
+if [ $# -eq 0 ]; then
+    echo "$(basename $0): Too few arguments"
+    usage
     exit 1
 fi
 
-#TODO make -noout default, add option to override
+POSITIONAL_ARGS=()
+while [ $# -gt 0 ]; do
+    arg="$1"
+    case $arg in
+        --print-csr)
+            PRINT_CSR=""
+            shift
+            ;;
+        -*)
+            echo "$(basename $0): Unrecognized option: $arg"
+            usage
+            exit 0
+            ;;
+        *)
+            POSITIONAL_ARGS+=($arg)
+            shift
+            ;;
+    esac
+done
+
+if [ "${#POSITIONAL_ARGS[@]}" -gt 1 ]; then
+    echo "$(basename $0): Too many arguments: ${POSITIONAL_ARGS[@]}"
+    usage
+    exit 1
+fi
+
+set -- "${POSITIONAL_ARGS[@]}"
+
+CSR="$1"
+
 openssl req \
     -verify \
+    $PRINT_CSR \
     -in $CSR
