93e30e65134a99ae150a186658b6896719dd4417
Kevin Matthews <kmatthews@cyberdata.net>
2018-06-01 15:34:12 -0700

    Server listens for connections

--- src/demo/ssl_server.c
+++ src/demo/ssl_server.c
@@ -1,4 +1,66 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SOCKETFD_INVALID        -1
+
+static int tcp_listen(unsigned int port)
+{
+    int socket_fd = SOCKETFD_INVALID;
+    struct sockaddr_in sin = {0};
+    int val = 1;
+    int ret;
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0)
+    {
+        perror("Failed to create socket");
+        return SOCKETFD_INVALID;
+    }
+
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(port);
+    ret = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+    if (ret < 0)
+    {
+        perror("Failed to set socket option");
+        close(socket_fd);
+        return SOCKETFD_INVALID;
+    }
+
+    ret = bind(socket_fd, (struct sockaddr *)&sin, sizeof(sin));
+    if (ret < 0)
+    {
+        perror("Failed to bind to socket");
+        close(socket_fd);
+        return SOCKETFD_INVALID;
+    }
+
+    ret = listen(socket_fd, 5);
+    if (ret < 0)
+    {
+        perror("Failed to listen on socket");
+        close(socket_fd);
+        return SOCKETFD_INVALID;
+    }
+
+    return socket_fd;
+}
+
 int main(void)
 {
+    int listen_socket = SOCKETFD_INVALID;
+
+    listen_socket = tcp_listen(8484);
+    if (listen_socket < 0)
+    {
+        fprintf(stderr, "Server failed to listen on socket. Exiting.\n");
+        return -1;
+    }
+
+    close(listen_socket);
+
     return 0;
 }
