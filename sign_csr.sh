d942729ff8b2d09aae93590db87719b9df52ee0e
Kevin Matthews <kmatthews@cyberdata.net>
2018-06-04 14:08:43 -0700

    Add script to sign CSR's

--- sign_csr.sh
+++ sign_csr.sh
@@ -8,6 +8,49 @@ usage ()
     echo ""
     echo "Sign a Certificate Signing Request using a Certifate Authority's self-signed certificate"
     echo ""
-    echo "$(basename $0) [CONFIG] [CSR] [KEY] [CRT]"
+    echo "$(basename $0) [CONFIG] [CA_KEY] [CSR] [CRT]"
     echo ""
+    echo "Read config, input a CSR, sign with the CA's private PEM, output a CRT"
 }
+
+if [ $# -lt 4 ]; then
+    echo "$(basename $0): Too few arguments"
+    usage
+    exit 1
+fi
+
+if [ $# -gt 4 ]; then
+    echo "$(basename $0): Too many arguments"
+    usage
+    exit 1
+fi
+
+CONFIG="$1"
+CA_KEY="$2"
+CSR="$3"
+CRT="$4"
+
+if [ -z $CONFIG ]; then
+    echo "$(basename $0): Invalid CONFIG"
+    exit 1
+fi
+if [ -z $CA_KEY ]; then
+    echo "$(basename $0): Invalid CA_KEY"
+    exit 1
+fi
+if [ -z $CSR ]; then
+    echo "$(basename $0): Invalid CSR"
+    exit 1
+fi
+if [ -z $CRT ]; then
+    echo "$(basename $0): Invalid CRT"
+    exit 1
+fi
+
+
+#openssl ca -config config/client.cnf -keyfile keys/ca.pem -in keys/client.csr -out keys/client.crt
+openssl ca \
+    -config $CONFIG \
+    -keyfile $CA_KEY \
+    -in $CSR \
+    -out $CRT
