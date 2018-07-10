d2fcd410095a501b411e222b2e0602e672581db4
Kevin Matthews <kmatthews@cyberdata.net>
2018-07-10 14:38:42 -0700

    Regenerate root CA in subdirectory

--- certs/chain/prep.sh
+++ certs/chain/prep.sh
@@ -3,5 +3,9 @@
 # Details are specified by root_ca.cnf
 mkdir newcerts
 mkdir private
-touch index.txt
-echo '00' > serial.txt
+mkdir intermediate
+mkdir root
+
+touch root/index.txt
+#TODO Only if file doesn't exist
+echo '00' > root/serial.txt
